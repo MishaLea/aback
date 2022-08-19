5d3e6bf293e0a54e3ae565a0ae9a6da619cb5817
MishaLea <mishafreestone@ymail.com>
2022-08-19 15:47:59 +0000

    add function and path for acounts app

--- project4/accounts/views.py
+++ project4/accounts/views.py
@@ -1,14 +1,19 @@
 from django.http import JsonResponse
+from django.http import Httpresponse
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from . import views
 
 def login(request):
-  userName = request.POST.get('user_name')
-  password = request.POST.get('password')
+  if request.method == "POST":
+    userName = request.POST.get['user_name']
+    password = request.POST.get['password']
   
-  user = authenticate(username=userName, password=password)
+    user = authenticate(username=userName, password=password)
   
-  if user is not None:
+    if user is not None:
+      login(request, user)
     return JsonResponse({"result": True})
   else:
     return JsonResponse({"result": False})
@@ -16,11 +21,21 @@ def login(request):
 
 
 def signup(request):
-  userName = request.POST.get('user_name')
-  userEmail = request.POST.get('email')
-  password = request.POST.get('password')
 
-  user = User.objects.create_user(userName, userEmail, password)
-  user.save()
+  if request.method == "POST":
+    userName = request.POST.get('user_name')
+    userEmail = request.POST.get('email')
+    password = request.POST.get('password')
+
+    user = User.objects.create_user(userName, userEmail, password)
+    user.name = user_name
+    user.email = email
+    user.password = password
+    
+    user.save()
+
+    messages.success(request, "Your account has been created successfully.")
+
+    return redirect("signup")
 
   return JsonResponse({"result": True})
