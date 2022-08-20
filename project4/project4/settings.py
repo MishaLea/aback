18766ef6f558be866fd42705ece25beced1d3bc5
MishaLea <mishafreestone@ymail.com>
2022-08-20 22:55:45 +0000

    add statement to '__init__' file

--- project4/project4/settings.py
+++ project4/project4/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b*z^evb4b)#3*ci2%gomd%w--r8wno$pcqw*823zyahw#-_kfm'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ project4/rooms/__init__.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import flask_sqlalchemy
+if os.path.exists("env.py"):
+    import env 
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+
+db = flask_SQLAlchemy(app)
+
+from taskmanager import routes #noqa 
